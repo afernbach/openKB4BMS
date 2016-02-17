498357671be2489fe52e8d2c1b4f181b6a8551d2
Igor Pelesic <ipelesic@auto.tuwien.ac.at>
2016-02-17 22:14:12 +0100

    add copyright

--- crawler/src/main/java/at/ac/tuwien/auto/sewoa/obix/ObixWatcher.java
+++ crawler/src/main/java/at/ac/tuwien/auto/sewoa/obix/ObixWatcher.java
@@ -1,3 +1,24 @@
+/*
+ *     openKB4BMS is an open source knowledge base (KB) acting as a
+ *     building management application.
+ *
+ *     Copyright (C) 2016
+ *     Institute of Computer Aided Automation, Automation Systems Group, TU Wien.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package at.ac.tuwien.auto.sewoa.obix;
 
 import at.ac.tuwien.auto.sewoa.http.HttpRetriever;
@@ -11,9 +32,6 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * Created by karinigor on 06.12.2015.
- */
 public class ObixWatcher {
 
     public static final String ADD_TEMPLATE =
--- crawler/src/main/java/at/ac/tuwien/auto/sewoa/obix/jena/device/TempSensorDeviceHandlerImpl.java
+++ crawler/src/main/java/at/ac/tuwien/auto/sewoa/obix/jena/device/TempSensorDeviceHandlerImpl.java
@@ -1,3 +1,24 @@
+/*
+ *     openKB4BMS is an open source knowledge base (KB) acting as a
+ *     building management application.
+ *
+ *     Copyright (C) 2016
+ *     Institute of Computer Aided Automation, Automation Systems Group, TU Wien.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package at.ac.tuwien.auto.sewoa.obix.jena.device;
 
 import at.ac.tuwien.auto.sewoa.obix.ObixUnitFactory;
@@ -13,9 +34,6 @@ import java.util.HashMap;
 
 import static at.ac.tuwien.auto.sewoa.obix.jena.ObixSewoaModelHandler.BASE_ONTOLOGY_URL;
 
-/**
- * Created by karinigor on 30.01.2016.
- */
 public class TempSensorDeviceHandlerImpl implements ObixDeviceHandler {
 
     protected HashMap<String, ObixIndividual> individuals;
